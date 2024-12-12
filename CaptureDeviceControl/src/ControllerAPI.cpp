9046a03be243acaea9c5d9cdffd4e5b70a4a3707
Chealemon <10639958+chealemon@user.noreply.gitee.com>
2024-12-12 15:31:42 +0800

    implement ControllerAPI Capture

--- CaptureDeviceControl/src/ControllerAPI.cpp
+++ CaptureDeviceControl/src/ControllerAPI.cpp
@@ -1,7 +1,9 @@
 #include "ControllerAPI.h"
+#include "TurntableScanner.h"
 
 static InitCallbackFunction g_InitImagesCapturedcallback = nullptr;
 static NextCallbackFunction g_NextImagesCapturedcallback = nullptr;
+static TurntableScanner g_scanner{};
 
 CONTROLLER_API void RegisterInitImagesCapturedCallback(InitCallbackFunction callback)
 {
@@ -17,10 +19,16 @@ CONTROLLER_API void RegisterNextImagesCapturedCallback(NextCallbackFunction call
 
 CONTROLLER_API void Capture()
 {
+	cv::Mat initImage1{ g_scanner.nextPicture() };
+	cv::Mat initImage2{ g_scanner.nextPicture() };
 	// when two image captured
-	// g_InitImagesCapturedcallback();
-	// 
-	// when next image captured
-	// g_NextImagesCapturedcallback();
+	g_InitImagesCapturedcallback(initImage1, initImage2);
+	 
+	while (g_scanner.getImgCount() > 0)
+	{
+		cv::Mat nextImage{ g_scanner.nextPicture() };
+		// when next image captured
+		g_NextImagesCapturedcallback(nextImage);
+	}
 	return;
 }
